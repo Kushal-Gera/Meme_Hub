6af1d5dd82ff97f22dd3212002217f018f6cc2b8
Kushal-Gera <kushalgera1212@gmail.com>
2019-10-25 21:18:43 +0530

    downloading enabled .....

--- app/src/main/java/kushal/application/memehub/BigMemeAct.java
+++ app/src/main/java/kushal/application/memehub/BigMemeAct.java
@@ -1,14 +1,19 @@
 package kushal.application.memehub;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import com.bumptech.glide.Glide;
 
@@ -28,15 +33,37 @@ public class BigMemeAct extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar2);
 
         final String url = getIntent().getStringExtra("image_url");
-        String title = getIntent().getStringExtra("title");
+        final String title = getIntent().getStringExtra("title");
 
         big_text.setText(title.toString());
         Glide.with(this).load(url).placeholder(R.drawable.placeholder).fitCenter().into(big_image);
 
-        big_image.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.download).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+
+                if (ActivityCompat.checkSelfPermission(BigMemeAct.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(BigMemeAct.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1234);
+                }
+                else {
+                    DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                    request.setTitle("Downloading Image: " + title);
+                    request.setDescription("Downloading an image file, please wait.");
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "Images");
+
+                    downloadManager.enqueue(request);
+                    Toast.makeText(BigMemeAct.this, "downloading...", Toast.LENGTH_SHORT).show();
+
+                }
+
 
             }
         });

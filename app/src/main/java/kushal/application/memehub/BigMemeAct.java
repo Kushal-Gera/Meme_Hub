99d93e6c4572f09ef14440a3d58861445ef79cc4
Kushal-Gera <kushalgera1212@gmail.com>
2019-10-26 10:36:42 +0530

    onRequestPermissionGranted !! added :P

--- app/src/main/java/kushal/application/memehub/BigMemeAct.java
+++ app/src/main/java/kushal/application/memehub/BigMemeAct.java
@@ -11,9 +11,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 
 import com.bumptech.glide.Glide;
 
@@ -22,6 +25,9 @@ public class BigMemeAct extends AppCompatActivity {
     ImageView big_image;
     Toolbar toolbar;
     TextView big_text;
+
+    String url = "";
+    String title = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,8 @@ public class BigMemeAct extends AppCompatActivity {
         big_text = findViewById(R.id.big_text);
         toolbar = findViewById(R.id.toolbar2);
 
-        final String url = getIntent().getStringExtra("image_url");
-        final String title = getIntent().getStringExtra("title");
+        url = getIntent().getStringExtra("image_url");
+        title = getIntent().getStringExtra("title");
 
         big_text.setText(title.toString());
         Glide.with(this).load(url).placeholder(R.drawable.placeholder).fitCenter().into(big_image);
@@ -50,24 +56,43 @@ public class BigMemeAct extends AppCompatActivity {
                             1234);
                 }
                 else {
-                    DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 
-                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-
-                    request.setTitle("Downloading Image: " + title);
-                    request.setDescription("Downloading an image file, please wait.");
-                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "Images");
-
-                    downloadManager.enqueue(request);
-                    Toast.makeText(BigMemeAct.this, "downloading...", Toast.LENGTH_SHORT).show();
+                    download(url, title);
 
                 }
-
 
             }
         });
 
+    }
 
+    private void download(String url, String title) {
+
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "Images");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setTitle("Downloading Image: " + title);
+        request.setDescription("Downloading an image file, please wait.");
+
+        downloadManager.enqueue(request);
+
+        Toast.makeText(BigMemeAct.this, "downloading...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            download(url, title);
+        }
+
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "Can't Download Without Permission Grants", Toast.LENGTH_SHORT).show();
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
